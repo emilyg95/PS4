5513817dd9c9540ca7a38ce7dd13114b0a8ff6ef
Emily Garner <emilyg1995@yahoo.com>
2018-02-18 13:15:06 -0600

    fixes to montgomery's code
    montgomery asked for a function that takes in the choice of door and location of the car, not to assign them within the function. So I got rid of the sampling within the function and made a separate function first to randomly select the placement of the car. Because the user gets to choose their own door I let this just be a numerical input into the second function and I got rid of x because it was unneccessary. I also renamed the variables and the function to be more clear. Then I simplified the if then statement to simply return true if the inputs of the choice of door = the placement of the car and to return false otherwise.

---
+++ PS4.R
@@ -0,0 +1,20 @@
+car_door = function(x){
+  x = sample(1:3, 1)
+  return(x)
+} ## a function that randomly selects a number between 1 and 3 for the placement of the car
+
+car1 = car_door("car1")
+
+car_pos = function(door_choice, car_door){ ## a function that takes as inputs the choice of door and the placement of the car
+  if (door_choice == car_door){  ## if then statement which says to return true if the choice of door equals the placement of the car and false otherwise
+    return(TRUE)
+    } 
+  else {
+    return(FALSE)
+  }
+}
+
+# Should return a TRUE if these samples are equal and
+# a false if they are not
+
+car_pos(2, car1) ## test
