5aaacd325f7974f0e74f3a98da085d4148c08c0e
Emily Garner <emilyg1995@yahoo.com>
2018-02-18 14:42:47 -0600

    setValidity
    first created a function called checkValidity which tested each input to see if it met the criteria I wanted it to (chosenDoor & carDoor must = 1, 2 or 3 & switch must = TRUE or FALSE) and returned an error message if any of these conditions did not hold. Then input that function into a setValidity function for objects of class door, which will not allow objects to be created if they do not meet the above criteria

--- PS4.R
+++ PS4.R
@@ -12,10 +12,7 @@ car_pos = function(door_choice, car_door){ ## a function that takes as inputs th
   else {
     return(FALSE)
   }
-}
-
-# Should return a TRUE if these samples are equal and
-# a false if they are not
+} ## returns TRUE if the inputs are equal and FALSE if they are not
 
 car_pos(2, car1) ## test
 
@@ -35,12 +32,38 @@ newDoor = function(x, y, z){ ## construction function takes in 3 arguments x, y,
   return(object) ## returns the object
 }
 
-newDoor(3,2,TRUE)
+newDoor(3,2,TRUE) ## check
 
+Door1 = newDoor(3,2,TRUE) ## defines an object I want to be valid
 
+Door2 = newDoor(3,7,TRUE) ## defines an object I want to be invalid
 
+checkValidity = function(object){ ## creates a function to check the validity of objects of class door
+  test1 = (object@chosenDoor == 1)
+  test2 = (object@chosenDoor == 2)
+  test3 = (object@chosenDoor == 3)
+  test4 = (object@carDoor == 1)
+  test5 = (object@carDoor == 2)
+  test6 = (object@carDoor == 3)
+  test7 = (object@switch == TRUE)
+  test8 = (object@switch == FALSE) ## a number of test statements defining valid inputs
+  if (!test1 & !test2 & !test3 | !test4 & !test5 & !test6 | !test7 & !test8){ ## if then statement which checks to see if the inputs x and y are both equal to 1 2 or 3 and input z is a boolean (TRUE/FALSE)
+    return("object is not a valid value")} ## returns true if the above holds and object is not a valid value if not
+    else{
+      return(TRUE)}
+}
 
+checkValidity(Door1) ## check on valid object
 
+checkValidity(Door2) ## check on invalid object
 
+setValidity("door", checkValidity) ## a function which only allows objects of class door to be created if they meet the criteria of my validity function above
 
+validObject(Door1) ## check on valid object
+
+validObject(Door2) ## check on invalid object 
+
+new("door", chosenDoor = 3, carDoor = 2, switch = TRUE) ## check on valid object
+
+new("door", chosenDoor = 3, carDoor = 6, switch = TRUE) ## check on invalid object 
 
